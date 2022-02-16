a2b13eaef3a52b9f897ef812a8360ca2c24238ea
Axel Olivier <axelolivier59@gmail.com>
2022-02-16 09:20:28 +0100

    reorg dossier

--- src/components/App/Home/HomeController.js
+++ src/components/App/Home/HomeController.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const LoginController = {
   login: (pseudo, password) => new Promise((succes, fail) => {
-    axios.post(`94.228.181.130:8081/login`, { pseudo, password })
+    axios.post(`http://localhost:8080/login/`, { pseudo, password })
       .then(({ data }) => {
         succes(data);
       })
